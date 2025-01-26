d37a375b1de454b30c4c4d4d6752c4ca4b98514e
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 13:44:09 +0530

    Add files via upload

---
+++ Arrays1d.java
@@ -0,0 +1,15 @@
+public class Arrays1d {
+    public static void main(String[]args){
+        int i;
+        int arr[]={1,2,3,4,5};
+        System.out.println("array elements are:");
+        for(i=0;i<5;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+        
+    
+
